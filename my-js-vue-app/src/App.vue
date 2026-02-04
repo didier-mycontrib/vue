feaa2429d5e217f7e42a6cda34fcfc5a46928d0a
Didier Defrance <didier@d-defrance.fr>
2026-02-04 11:25:07 +0100

    my-js-vue-app v1

---
+++ my-js-vue-app/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+   import Calculatrice from "./Calculatrice.vue";
+   let msg="hello world";
+</script>
+
+<template>
+  <h1>My Vue App</h1>
+  <p class="c1"> {{ msg }}</p>
+  <hr/>
+  <calculatrice></calculatrice>
+</template>
+
+<style scoped>
+   .c1{
+    color:red;
+   }
+</style>
---
+++ my-js-vue-app/src/Calculatrice.vue
@@ -0,0 +1,23 @@
+<script setup>
+   import { ref } from 'vue';
+   let a=0;
+   let b=0;
+   let res=ref(0);
+
+   function add(){
+      res.value= a+b;
+      console.log(`add() with a=${a} b=${b} res=${res}`)
+   }
+</script>
+
+<template>
+  <h1>Calculatrice</h1>
+  <p> a: <input type="number" v-model="a" /></p>
+  <p> b: <input type="number" v-model="b" /></p>
+  <p><button  @click="add">addition</button></p>
+  <p> res: {{ res }}</p>
+</template>
+
+<style scoped>
+
+</style>
